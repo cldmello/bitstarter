61d28d2662c12cbe68640bad4f4c0ea5fd82ba21
AWS Ubuntu User <ubuntu@git.com>
2013-07-07 04:02:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,14 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var file = './index.html';
+var filename = 'index.html';
+var htmlBuf = new Buffer(fs.readFileSync(filename), 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send( function(file) {
-            var html = fs.readFileSync(file);
-            return new Buffer(html).toString('utf-8');
-            }
-       );
+  response.send(htmliBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
